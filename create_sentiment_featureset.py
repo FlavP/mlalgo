548bf1f56fcc4bd6eb350f18c21d9f91cb1cd7bb
flavius <flavius.popoiu@mediaimage.ro>
2016-10-28 19:58:56 +0300

    almost there

--- create_sentiment_featureset.py
+++ create_sentiment_featureset.py
@@ -7,9 +7,27 @@ import pickle
 from collections import Counter
 
 lemmi = WordNetLemmatizer()
-hm_lines = 10000000
-<<<<<<< HEAD
-#uite un comentariu
-=======
+hm_lines = 100000
 
->>>>>>> f2f97c65537166b4a70621c370486ebe74e574f5
+def create_lexicon(pos, neg):
+    lexicon = []
+    with open(pos, 'r') as f:
+        contents = f.readlines()
+        for l in contents[:hm_lines]:
+            all_words = word_tokenize(l)
+            lexicon += list(all_words)
+    with open(neg, 'r') as f:
+        contents = f.readlines()
+        for l in contents[:hm_lines]:
+            all_words = word_tokenize(l)
+            lexicon += list(all_words)
+    lexicon = [lemmi.lemmatize(i) for i in lexicon]
+    w_count = Counter(lexicon)
+    #print(w_count)
+    l2 = []
+    for w in w_count:
+        #print(w_count[w])
+        if 1000 > w_count[w] > 50:
+            l2.append(w)
+    #print(w)
+    return l2
