3c100341a2b5273559290ec39a88a726653d7514
flavius <flavius.popoiu@mediaimage.ro>
2016-11-04 15:13:49 +0200

    Convolutional

--- ConvNN.py
+++ ConvNN.py
@@ -8,9 +8,11 @@ batch_size = 128
 
 x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
+keep_rate = 0.8
+keep_prob = tf.placeholder(tf.float32)
 
 def conv2d(x, W):
-    return tf.nn.convd(x, W, strides=[1,1,1,1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
 
 def maxpool2d(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
@@ -25,13 +27,14 @@ def convolutional_neural_network(x):
               'b_fully': tf.Variable(tf.random_normal([1024])),
               'out': tf.Variable(tf.random_normal([n_classes])),}
     x = tf.reshape(x, shape=[-1, 28, 28, 1])
-    conv1 = conv2d(x, weights['W_conv1'])
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
     conv1 = maxpool2d(conv1)
-    conv2 = conv2d(conv1, weights['W_conv2'])
+    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
     conv2 = maxpool2d(conv2)
 
     fc = tf.reshape(conv2,[-1, 7*7*64])
     fc = tf.nn.relu(tf.matmul(fc, weights['W_fully']) + biases['b_fully'])
+    fc = tf.nn.dropout(fc, keep_rate)
     output = tf.matmul(fc, weights['out']) + biases['out']
     return output
 
